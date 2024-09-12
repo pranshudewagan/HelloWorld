86406b1fb567daa0b639eda0eedabd3684acf34a
pdewagan <65984636+pranshudewagan@users.noreply.github.com>
2024-09-12 14:05:09 -0500

    added ImageView var

--- app/src/main/java/com/cs407/helloworld/MainActivity.kt
+++ app/src/main/java/com/cs407/helloworld/MainActivity.kt
@@ -2,9 +2,16 @@ package com.cs407.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    // Declare an ImageView Variable
+    private lateinit var ingView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
